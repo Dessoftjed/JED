c76b38a0b48a7150f9c93d7084186b83143fd29e
hiuta <72161093+hiuta@users.noreply.github.com>
2021-01-22 16:44:13 -0600

    Cambio Text

--- Views/modules/usuarios/salir.php
+++ Views/modules/usuarios/salir.php
@@ -1,8 +1,8 @@
 <?php session_destroy(); ?>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-3">Haz Salido de la Aplicacion Suerte.!</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <h1 class="display-3">Sesion Cerrado con exito.!</h1>
+    <p class="lead">Vuelve pronto !.</p>
   </div>
 </div>
 
